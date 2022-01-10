c126749079564c1fd2f266340d870a4358610f53
Júlio Cesar <juliocesar27072004@outlook.com>
2022-01-10 13:18:19 -0300

    Project Created

---
+++ backend/src/main/java/com/astuno/moviecatalog/MoviecatalogApplication.java
@@ -0,0 +1,13 @@
+package com.astuno.moviecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/astuno/moviecatalog/MoviecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astuno.moviecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
