d20126567873bd12185319daa471b134e155d87f
Júlio Cesar <juliocesar27072004@outlook.com>
2022-01-10 15:31:46 -0300

    Form Layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,42 @@
+import "./styles.css";
+
 function Form() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return (
-        <h1>Form Page</h1>
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Type your best email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Type your Review</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Save</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary dsmovie-btn mt-3">Annul</button>
+            </div >
+        </div >
     )
 }
 
