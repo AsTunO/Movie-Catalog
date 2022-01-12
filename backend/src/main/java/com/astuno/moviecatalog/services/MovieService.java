9247fd068a120d5cd17ec44d0e7db75b13919a63
Júlio Cesar <juliocesar27072004@outlook.com>
2022-01-12 15:35:28 -0300

    First homolog

--- backend/src/main/java/com/astuno/moviecatalog/services/MovieService.java
+++ backend/src/main/java/com/astuno/moviecatalog/services/MovieService.java
@@ -1,4 +1,4 @@
-package com.astuno.moviecatalog.services;
+	package com.astuno.moviecatalog.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
