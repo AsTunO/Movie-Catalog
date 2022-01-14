f7357e8ba300ca944ec7717e022bdab973a4c136
Júlio Cesar <juliocesar27072004@outlook.com>
2022-01-14 10:33:13 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
+import { BASE_URL } from "../../utils/requests";
 
 function Listing() {
+
+
+    // Mistake
+    axios.get(`${BASE_URL}/movies`)
+        .then(response => {
+            console.log(response.data);
+        })
+
     return (
         <>
             <Pagination />
